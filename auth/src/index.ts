abdb2a7b317ed2fe49e6ea3e7533ed5b74b291cc
VinaKale1998 <vinaykale1999@gmail.com>
2023-12-16 17:05:33 +0530

    initilazing cookie-session capability

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 //to allow async error throws to be handled by express directly rather than having to send it off with next();
 import "express-async-errors";
 import dotenv from "dotenv";
@@ -9,13 +10,22 @@ import { signInRouter } from "./routes/signin";
 import { signUpRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./Errors/not-found-error";
+
 import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+app.set("trust proxy", true);
 app.use(express.json());
 app.use(cors());
+
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signInRouter);
