f0815bf1227dac12982cdaf34dacaa497198955d
VinaKale1998 <vinaykale1999@gmail.com>
2024-01-18 10:57:05 +0530

    package updated |  Expiration event publisher set up

--- expiration/src/queues/expiration-queue.ts
+++ expiration/src/queues/expiration-queue.ts
@@ -1,4 +1,6 @@
 import Queue from "bull";
+import { ExpirationCompletePublisher } from "../events/publishers/expiration-complete-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 interface Payload {
   orderId: string;
@@ -11,10 +13,9 @@ const expirationQueue = new Queue<Payload>("Order:expiration", {
 
 expirationQueue.process(async (job) => {
   try {
-    console.log(
-      "I want to publish an expiration:complete event for orderId",
-      job.data.orderId
-    );
+    new ExpirationCompletePublisher(natsWrapper.client).publish({
+      orderId: job.data.orderId,
+    });
   } catch (err) {
     console.log(err);
   }
